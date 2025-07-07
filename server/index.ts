7468dcb2102252c80fa92a1615c53c5ef7621326
bardinp4v <44719150-bardinp4v@users.noreply.replit.com>
2025-07-07 16:00:42 +0000

    Restored to 'cdbc276c8709194f31b56a7a75d3de104e55aac1'
    Replit-Restored-To: cdbc276c8709194f31b56a7a75d3de104e55aac1

--- server/index.ts
+++ server/index.ts
@@ -61,9 +61,10 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // serve the app on configurable port (default 3000)
+  // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
-  const port = parseInt(process.env.PORT || "3000");
+  // It is the only port that is not firewalled.
+  const port = 5000;
   server.listen({
     port,
     host: "0.0.0.0",
